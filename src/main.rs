b44baac8083e202c7d18ebda088eee3da529e159
Atlanswer <atlanswer@gmail.com>
2025-03-17 20:30:10 +0800

    learn

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let _foo = "Rust";
+    let mut _bar = "Analyzer";
+
+    print!("Hi");
+    println!("Hi");
+    eprint!("Hi");
+    eprintln!("Hi");
+
+    let _flag = true;
+    let _flag2: bool = true;
+
+    let _x = 5;
+    let _x2: i64 = -5;
+    let _x3: u32 = 5;
+
+    let _y = 3.14;
+    let _y2: f32 = 1.23;
+
+    let _c: char = '✅';
+
+    let _t: (char, i32) = ('A', 5);
+    let (_l, _r) = _t;
+}
